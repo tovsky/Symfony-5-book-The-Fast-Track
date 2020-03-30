e86d9215816ef634aac1e1f70a4292ecdab39c28
tovsky <pavel.hr@mail.ru>
2020-03-31 01:11:46 +0300

    Added method generate slug.

--- src/Entity/Conference.php
+++ src/Entity/Conference.php
@@ -6,6 +6,7 @@ use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+use Symfony\Component\String\Slugger\SluggerInterface;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\ConferenceRepository")
@@ -58,6 +59,18 @@ class Conference
     public function getId(): ?int
     {
         return $this->id;
+    }
+
+    /**
+     * Method creat slug if slug empty or === '-'
+     *
+     * @param SluggerInterface $slugger
+     */
+    public function computeSlug(SluggerInterface $slugger)
+    {
+        if (!$this->slug || '-' === $this->slug) {
+            $this->slug = (string) $slugger->slug((string) $this)->lower();
+        }
     }
 
     public function getCity(): ?string
