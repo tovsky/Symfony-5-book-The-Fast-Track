0ff682886d0d28574072e375422ef7aa9b9f8356
tovsky <pavel.hr@mail.ru>
2020-04-02 14:27:39 +0300

    Added CommentFormType

--- src/Controller/ConferenceController.php
+++ src/Controller/ConferenceController.php
@@ -2,7 +2,9 @@
 
 namespace App\Controller;
 
+use App\Entity\Comment;
 use App\Entity\Conference;
+use App\Form\CommentFormType;
 use App\Repository\CommentRepository;
 use App\Repository\ConferenceRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -40,6 +42,10 @@ class ConferenceController extends AbstractController
         CommentRepository $commentRepository,
         ConferenceRepository $conferenceRepository
     ) {
+        $comment = new Comment();
+        $form = $this->createForm(CommentFormType::class, $comment);
+
+
         $offset = max(0, $request->query->getInt('offset', 0));
         $paginator = $commentRepository->getCommentPaginator($conference, $offset);
 
@@ -48,6 +54,7 @@ class ConferenceController extends AbstractController
             'comments' => $paginator,
             'previous' => $offset - CommentRepository::PAGINATOR_PER_PAGE,
             'next' => min(count($paginator), $offset + CommentRepository::PAGINATOR_PER_PAGE),
+            'comment_form' => $form->createView(),
         ]));
     }
 }
