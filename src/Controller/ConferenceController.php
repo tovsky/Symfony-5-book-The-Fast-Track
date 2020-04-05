0a3e2d52d4d5b0af2ebc382f3ebebee09efaffc2
tovsky <pavel.hr@mail.ru>
2020-04-06 02:31:56 +0300

    Added type fields form and added validate rules.

--- src/Controller/ConferenceController.php
+++ src/Controller/ConferenceController.php
@@ -7,6 +7,7 @@ use App\Entity\Conference;
 use App\Form\CommentFormType;
 use App\Repository\CommentRepository;
 use App\Repository\ConferenceRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -19,10 +20,17 @@ class ConferenceController extends AbstractController
      * @var Environment
      */
     private $twig;
+    /**
+     * @var EntityManagerInterface
+     */
+    private $entityManager;
 
-    public function __construct(Environment $twig)
-    {
+    public function __construct(
+        Environment $twig,
+        EntityManagerInterface $entityManager
+    ) {
         $this->twig = $twig;
+        $this->entityManager = $entityManager;
     }
 
     /**
@@ -44,7 +52,15 @@ class ConferenceController extends AbstractController
     ) {
         $comment = new Comment();
         $form = $this->createForm(CommentFormType::class, $comment);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $comment->setConference($conference);
 
+            $this->entityManager->persist($comment);
+            $this->entityManager->flush();
+
+            return $this->redirectToRoute('conference', ['slug' => $conference->getSlug()]);
+        }
 
         $offset = max(0, $request->query->getInt('offset', 0));
         $paginator = $commentRepository->getCommentPaginator($conference, $offset);
