3b70b4bf784b3bd4f2e2a9dba782fe4140833057
tovsky <pavel.hr@mail.ru>
2020-05-08 18:09:04 +0300

    Added cache for proxy-server.

--- public/index.php
+++ public/index.php
@@ -21,6 +21,11 @@ if ($trustedHosts = $_SERVER['TRUSTED_HOSTS'] ?? $_ENV['TRUSTED_HOSTS'] ?? false
 }
 
 $kernel = new Kernel($_SERVER['APP_ENV'], (bool) $_SERVER['APP_DEBUG']);
+
+if ('dev' === $kernel->getEnvironment()) {
+    $kernel = new Symfony\Bundle\FrameworkBundle\HttpCache\HttpCache($kernel);
+}
+
 $request = Request::createFromGlobals();
 $response = $kernel->handle($request);
 $response->send();
